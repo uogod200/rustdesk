49dd95f32eccfc30f98896b6bd19f94e49f25a19
uogod <uogod@163.com>
2025-08-22 15:45:58 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1014,7 +1014,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://www.uogod.top:21114".to_owned()
 }
 
 #[inline]
